29e25713fa23411749dfb9dae86f303b5a2f22b7
weng7533 <weng7533@users.noreply.github.com>
2019-11-15 12:49:36 -0500

    MVP

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import CharacterCard from './CharacterCard';
-
+import SearchForm from './SearchForm';
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [data, setData] = useState([]);
@@ -23,15 +23,25 @@ export default function CharacterList() {
 
   }, [query]);
 
+  const searchHandler = (event) => {
+    return setQuery(event.target.value);
+  }
 
   return (
-    <section className="character-list">
-      {
-        data.map((character, index) => {
-          return <CharacterCard character={character} key={index} />
-        })
+    <>
 
-      }
-    </section>
+      <SearchForm searchHandler={searchHandler} />
+
+
+      <section className="character-list">
+
+        {
+          data.map((character, index) => {
+            return <CharacterCard character={character} key={index} />
+          })
+        }
+      </section>
+
+    </>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
- 
+export default function SearchForm(props) {
+
   return (
     <section className="search-form">
-     // Add a search form here
+
+      <form className="search">
+        <input
+          type="text"
+          onChange={props.searchHandler}
+
+          name="name"
+          tabIndex="0"
+          className="prompt search-name"
+          placeholder="search by name"
+          autoComplete="off"
+        />
+      </form>
     </section>
   );
 }
